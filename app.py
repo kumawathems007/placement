ad7fe9bf4e0392e1a1762e6f878c5483e08a3178
kumawatsardar007 <84585243+kumawatsardar007@users.noreply.github.com>
2022-08-07 12:03:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+from flask_ngrok import run_with_ngrok
+import pickle
+
+
+app = Flask(__name__)
+model = pickle.load(open('/content/drive/MyDrive/kn1_model.pkl', 'rb'))
+model = pickle.load(open('/content/drive/MyDrive/nb1_model.pkl', 'rb'))
+model = pickle.load(open('/content/drive/MyDrive/dt1_model.pkl', 'rb'))
+model = pickle.load(open('/content/drive/MyDrive/rf1_model.pkl', 'rb'))
+model = pickle.load(open('/content/drive/MyDrive/li1_model.pkl', 'rb')) 
+run_with_ngrok(app)
+
+@app.route('/')
+def home():
+  
+    return render_template("index2.html")
+  
+@app.route('/predict',methods=['GET'])
+def predict():
+    
+    
+    '''
+    For rendering results on HTML GUI
+    '''
+    _10th = float(request.args.get('_10th'))
+    _12th = float(request.args.get('_12th'))
+    B_tech = float(request.args.get('B_tech'))
+    _7_sem = float(request.args.get('_7_sem'))
+    _6_sem =float(request.args.get('_6_sem'))
+    _5_sem = float(request.args.get('_5_sem'))
+    final_performance = float(request.args.get('final_performance'))
+    Medium = float(request.args.get('Medium'))
+    
+    prediction = model.predict([[_10th, _12th,B_tech,_7_sem,_6_sem,_5_sem,final_performance,Medium]])
+    
+        
+    return render_template('index2.html', prediction_text='All Model  has predicted Placment for given Data is : {}'.format(prediction))
+   
+app.run()
